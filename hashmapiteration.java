2f5a5a90c5363c4ce64e31db337dc5832f8db6dc
AryanMahajan <mahajanaryan0409@gmail.com>
2024-04-13 15:23:34 +0530

    hashmap iteration

--- hashmapiteration.java
+++ hashmapiteration.java
@@ -1,5 +1,21 @@
 import java.util.*;
 
 public class hashmapiteration {
+    public static void main(String[] args) {
+        HashMap<String,Integer> map = new HashMap<>();
+        map.put("USA",200);
+        map.put("UK",175);
+        map.put("India",300);
+        map.put("China",350);
+        map.put("Indonesia", 2);
+
+        Set<String> keys = map.keySet();
+
+        //Iteration
+        for (String key : keys) {
+            System.out.println("Key = "+key+", Value = "+map.get(key));
+        }
+    }
+
     
 }

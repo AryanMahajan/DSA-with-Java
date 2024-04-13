6758e4095779827482806d5b0d8d8c63cc2fba64
AryanMahajan <mahajanaryan0409@gmail.com>
2024-04-13 14:58:24 +0530

    hashmaps

--- hashmap.java
+++ hashmap.java
@@ -5,9 +5,23 @@ public class hashmap {
         //country , Population
         HashMap<String, Integer> map = new HashMap<>(); 
 
-        //insertion
+        //insertion - O(1)
         map.put("India", 1);
         map.put("China", 2);
         map.put("US", 3);
+        System.out.println(map);
+
+        //update - O(1)
+        map.put("US", 5);
+        System.out.println(map);
+
+        //Search - O(1)
+        System.out.println(map.get("India"));           //India was the kkey so it will return the value
+        System.out.println(map.get(1));                 //1 is not the key so it will return null
+
+        //Delete - O(1)
+        map.remove("US");
+        System.out.println(map);
+
     }
 }
